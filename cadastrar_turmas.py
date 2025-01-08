5635c8d2e44a75135b74a792080bde032f5b476b
Joao Pedro <borgesjpbd@gmail.com>
2025-01-08 00:21:17 -0400

    removi a lista vazia do modulo cadastrar turmas, e coloquei para carregar os dados utilizando o pickle não tinha visto esse erro antes, adicionei as DocStrings das maiorias da funções criadas para facilitar o entendimento

--- cadastrar_turmas.py
+++ cadastrar_turmas.py
@@ -1,24 +1,46 @@
 import random
 import pandas as pd
-from cadastrar_alunos import alunos 
-from cadastrar_professor import professores 
-from cadastrar_disciplina import disciplinas  
+from save import carregar_dados, salvar_dados
 
-turmas = []
+
+# Carregar dados dos arquivos pickle
+turmas = carregar_dados('turmas.pkl')
+disciplinas = carregar_dados('disciplinas.pkl')
+professores = carregar_dados('professores.pkl')
+alunos = carregar_dados('alunos.pkl')
 
 def buscar_disciplina(codigo_disciplina):
+    
+    """
+    Busca uma disciplina pelo código.
+    
+    codigo_disciplina (int): Código da disciplina.
+    
+ Retorna A disciplina encontrada ou None se não encontrada.
+    """
     for disciplina in disciplinas:
         if disciplina['código'] == codigo_disciplina:
             return disciplina
     return None
 
 def buscar_professor(codigo_professor):
+    """
+    Busca um professor pelo código.
+    Retorna
+    O professor encontrado ou None se não encontrado.
+    """
     for professor in professores:
         if professor['Id'] == codigo_professor:
             return professor
     return None
 
 def buscar_alunos(codigos_alunos):
+    """
+    Busca alunos pelos códigos de RA.
+    
+    Lista de códigos de RA dos alunos.
+    Retorna Lista de alunos encontrados.
+    """
     alunos_encontrados = []
     for codigo in codigos_alunos:
         for aluno in alunos:
@@ -28,6 +50,11 @@ def buscar_alunos(codigos_alunos):
     return alunos_encontrados
 
 def cadastro_turma():
+    """
+    Solicita informações da turma ao usuário, gera um código aleatório,
+    e salva os dados da turma em um arquivo pickle.
+    """
+    
     nome_turma = input("Digite o nome da turma: ")
     codigo_disciplina = int(input("Digite o código da disciplina: "))
     disciplina = buscar_disciplina(codigo_disciplina)
@@ -57,18 +84,15 @@ def cadastro_turma():
         'alunos': alunos_turma
     }
 
-    turmas.append(turma)
-    print(f"Turma cadastrada com sucesso! Código da turma: {codigo_turma}")
-    df = pd.DataFrame([{
-        'Código da Turma': turma['codigo_turma'],
-        'Nome da Turma': turma['nome_turma'],
-        'Disciplina': turma['disciplina']['NomeDisciplina'],
-        'Professor': turma['professor']['Nome'],
-        'Alunos': ', '.join([aluno['Nome'] for aluno in turma['alunos']])
-    }])
-    print(df.to_string(index=False, justify='left'))
+    salvar_dados('turmas.pkl', turmas)  # Salvar os dados no arquivo pickle
+    print(f"Turma {nome_turma} cadastrada com sucesso! Código gerado: {codigo_turma}")
+    
 
 def listar_turmas():
+    """
+    Mostra a lista de turmas cadastradas em formato de tabela.
+    Se não tiver nenhuma turma cadastrada, exibe uma mensagem informando que nenhuma turma foi cadastrada.
+    """
     if turmas:
         df = pd.DataFrame([{
             'Código da Turma': turma['codigo_turma'],
